baf321e37b1eaf37a79c8c52141fcf05a4360136
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2018-10-13 15:54:15 +0530

    updated by gaurav

--- resources/script.js
+++
@@ -1,7 +0,0 @@
-function myFunction(imgs) {
-    var expandImg = document.getElementById("expandedImgc");
-    var imgText = document.getElementById("imgtextc");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-}
